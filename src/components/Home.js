4a5060bc803e2e55f5a5dba0f4552e5738a47c1e
Valentin Le Bastard <valentinlebastard@gmail.com>
2021-06-25 15:16:01 +0200

    Page d'accueil

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import Appbar from "./Appbar";
+
+function Home() {
+    return (
+        <div className="Home">
+            <Appbar />
+            <h1>EBIOS HOME PAGE</h1>
+        </div >
+    );
+}
+
+export default Home;
