0489b3d21693dd2ba9df773c4fd98afc27e0e885
Valentin Le Bastard <valentinlebastard@gmail.com>
2021-06-25 15:15:43 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact from="/" component={Home} />
+          <Route exact from="/Signin" component={Signin} />
+          <Route exact from="/Signup" component={Signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
