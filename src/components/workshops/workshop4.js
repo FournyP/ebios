4341fe5fc44fc46c799d22c1497a7b8fc13d7c0d
Valentin Le Bastard <valentinlebastard@gmail.com>
2021-06-25 16:06:26 +0200

    Ajout des workshops

---
+++ src/components/workshops/workshop4.js
@@ -0,0 +1,9 @@
+function Workshop4() {
+    return (
+        <div className="Home">
+            <h1>ATELIER 4</h1>
+        </div >
+    );
+}
+
+export default Workshop4;
---
+++ src/components/workshops/workshop5.js
@@ -0,0 +1,9 @@
+function Workshop5() {
+    return (
+        <div className="Home">
+            <h1>ATELIER 5</h1>
+        </div >
+    );
+}
+
+export default Workshop5;
