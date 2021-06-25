10f4967dbc2769fc1cb8ce215ba9cd8bb0650e24
Valentin Le Bastard <valentinlebastard@gmail.com>
2021-06-25 16:06:40 +0200

    Rooting

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/Home';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
-
-
+import Project from './components/Project';
 
 function App() {
   return (
@@ -14,6 +13,7 @@ function App() {
           <Route exact from="/" component={Home} />
           <Route exact from="/Signin" component={Signin} />
           <Route exact from="/Signup" component={Signup} />
+          <Route exact from="/Project" component={Project} />
         </Switch>
       </Router>
     </div>
