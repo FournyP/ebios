cf602a2dc09d0c0f43cb86065c97a5b9a485c013
Valentin Le Bastard <valentinlebastard@gmail.com>
2021-07-02 11:15:41 +0200

    Table Workshop 2

--- assets/components/workshops/workshop2.js
+++ assets/components/workshops/workshop2.js
@@ -1,10 +1,150 @@
-import React from "react";
+import React, { useState } from 'react';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import { v4 as uuidv4 } from 'uuid';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+        },
+    },
+    button: {
+        margin: theme.spacing(1),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 130,
+    },
+    faibleMenu: {
+        color: "blue",
+    }
+}))
 
 function Workshop2() {
+    const classes = useStyles()
+    const [inputFields, setInputFields] = useState([
+        { id: uuidv4(), firstName: '', lastName: '' },
+    ]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("InputFields", inputFields);
+    };
+
+    const handleChangeInput = (id, event) => {
+        const newInputFields = inputFields.map(i => {
+            if (id === i.id) {
+                i[event.target.name] = event.target.value
+            }
+            return i;
+        })
+
+        setInputFields(newInputFields);
+    }
+
+    const handleAddFields = () => {
+        setInputFields([...inputFields, { id: uuidv4(), firstName: '', lastName: '' }])
+    }
+
+    const handleRemoveFields = id => {
+        const values = [...inputFields];
+        values.splice(values.findIndex(value => value.id === id), 1);
+        setInputFields(values);
+    }
     return (
-        <div className="Home">
-            <h1>ATELIER 2</h1>
-        </div >
+        <Container>
+            <form className={classes.root} onSubmit={handleSubmit}>
+                {inputFields.map(inputField => (
+                    <div key={inputField.id}>
+                        <TextField
+                            name="firstName"
+                            label="Source de risque"
+                            variant="filled"
+                            value={inputField.firstName}
+                            onChange={event => handleChangeInput(inputField.id, event)}
+                        />
+                        <TextField
+                            name="lastName"
+                            label="Objectif Visé"
+                            variant="filled"
+                            multiline
+                            value={inputField.lastName}
+                            onChange={event => handleChangeInput(inputField.id, event)}
+                        />
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-label">Motivation</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                            >
+                                <MenuItem value={'+'}>+</MenuItem>
+                                <MenuItem value={'++'}>++</MenuItem>
+                                <MenuItem value={'+++'}>+++</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-label">Ressources</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                            >
+                                <MenuItem value={'+'}>+</MenuItem>
+                                <MenuItem value={'++'}>++</MenuItem>
+                                <MenuItem value={'+++'}>+++</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-label">Activité</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                            >
+                                <MenuItem value={'+'}>+</MenuItem>
+                                <MenuItem value={'++'}>++</MenuItem>
+                                <MenuItem value={'+++'}>+++</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-label">Pertinence</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                            >
+                                <MenuItem value={'faible'} className={classes.faibleMenu}>Faible</MenuItem>
+                                <MenuItem value={'moyenne'}>Moyenne</MenuItem>
+                                <MenuItem value={'élevée'}>Élevée</MenuItem>
+                            </Select>
+                        </FormControl>
+
+
+                        <IconButton disabled={inputFields.length === 1} onClick={() => handleRemoveFields(inputField.id)}>
+                            <RemoveIcon />
+                        </IconButton>
+                        <IconButton onClick={handleAddFields}>
+                            <AddIcon />
+                        </IconButton>
+                    </div>
+                ))}
+                <Button
+                    className={classes.button}
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    onClick={handleSubmit}
+                >Send</Button>
+            </form>
+        </Container>
     );
 }
 
