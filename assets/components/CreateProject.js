d5c876ef378f34de2200fa7c66f485c15f614020
FournyP <pierre.fourny76@gmail.com>
2021-07-04 18:03:43 +0200

    Update CreateProject to adding the organization

--- assets/components/CreateProject.js
+++ assets/components/CreateProject.js
@@ -1,21 +1,27 @@
 import React from "react";
 import Appbar from "./Appbar";
-import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
-import InputLabel from '@material-ui/core/InputLabel';
-import Tableau from '../resources/Tableau-Create-Project.png';
-import { makeStyles } from '@material-ui/core/styles';
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Button from "@material-ui/core/Button";
+import InputLabel from "@material-ui/core/InputLabel";
+import Tableau from "../resources/Tableau-Create-Project.png";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  table: {
-    maxWidth: 600,
-    marginTop: theme.spacing(1),
-  }
-}));
+async function initOptions() {
+  let request = new Request(process.env.API_URL + "api/organizations", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return await (await fetch(request)).json();
+}
 
 async function handleSubmit(parameters) {
   const request = new Request(process.env.API_URL + "api/projects", {
@@ -34,14 +40,82 @@ async function handleSubmit(parameters) {
   });
 }
 
-function CreateProject() {
-  const classes = useStyles();
+const CreateProject = () => {
+  const classes = makeStyles((theme) => ({
+    table: {
+      maxWidth: 600,
+      marginTop: theme.spacing(1),
+    },
+  }))();
+
+  const [options, setOptions] = React.useState([]);
+  const loading = open && options.length === 0;
+
+  React.useEffect(async() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    let options = await initOptions();
+
+    if (active) {
+      for(option in options["hydra:member"])
+      {
+        setOptions(option.name);
+      }
+    }
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
   return (
     <div className={classes.bg}>
       <Appbar />
       <Container component="main" maxWidth="md">
         <h1>Créer un nouveau projet</h1>
         <form onSubmit={handleSubmit}>
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel id="organization"> Organization </InputLabel>
+            <Autocomplete
+              id="organization"
+              style={{ width: 300 }}
+              open={open}
+              onOpen={() => {
+                setOpen(true);
+              }}
+              onClose={() => {
+                setOpen(false);
+              }}
+              getOptionSelected={(option, value) => option.name === value.name}
+              getOptionLabel={(option) => option.name}
+              options={options}
+              loading={loading}
+              fullWidth
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Organization"
+                  variant="outlined"
+                  fullWidth
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <React.Fragment>
+                        {loading ? (
+                          <CircularProgress color="inherit" size={20} />
+                        ) : null}
+                        {params.InputProps.endAdornment}
+                      </React.Fragment>
+                    ),
+                  }}
+                />
+              )}
+            />
+          </FormControl>
           <TextField
             variant="outlined"
             margin="normal"
@@ -104,6 +178,6 @@ function CreateProject() {
       </Button>
     </div>
   );
-}
+};
 
 export default CreateProject;
