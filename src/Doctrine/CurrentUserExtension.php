483a697205e26ee071b71526232be334a7b902c7
FournyP <pierre.fourny76@gmail.com>
2021-07-04 18:05:30 +0200

    Starting to implement a class to get only rows in the database who interest the user

---
+++ src/Doctrine/CurrentUserExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Doctrine;
+
+use Symfony\Component\Security\Core\Security;
+use Doctrine\ORM\QueryBuilder;
+use App\Entity\Organization;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+
+class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?string $operationName = null)
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, ?string $operationName = null, array $context = [])
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass)
+    {
+        $alias = $queryBuilder->getRootAliases()[0];
+
+        if ($resourceClass === Organization::class) {
+            $queryBuilder->andWhere("$alias.user = :current_user")
+                ->setParameter('user', $this->security->getUser());
+        }
+    }
+} 
