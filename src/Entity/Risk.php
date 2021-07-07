ea869d3e90af594bd0fcc20d29adb8bb822be1a4
FournyP <pierre.fourny76@gmail.com>
2021-07-07 15:56:13 +0200

    Adding search for risk

--- src/Entity/Risk.php
+++ src/Entity/Risk.php
@@ -7,7 +7,9 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use App\Repository\RiskRepository;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
 
 /**
  * @ORM\Entity(repositoryClass=RiskRepository::class)
@@ -15,7 +17,8 @@ use ApiPlatform\Core\Annotation\ApiResource;
 #[ApiResource(
     normalizationContext: ['groups' => ['read:Risk']],
     denormalizationContext: ['groups' => ['write:Risk']]
-)]
+),
+ApiFilter(SearchFilter::class, properties: ['workshop2' => "exact"])]
 class Risk
 {
     /**
