13388a3eb8eb9f1601ef3422dc4ec3a5d2e5550a
FournyP <pierre.fourny76@gmail.com>
2021-07-07 15:46:53 +0200

    Adding filter to Action

--- src/Entity/Action.php
+++ src/Entity/Action.php
@@ -7,7 +7,9 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use App\Repository\ActionRepository;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
 
 /**
  * @ORM\Entity(repositoryClass=ActionRepository::class)
@@ -15,7 +17,8 @@ use ApiPlatform\Core\Annotation\ApiResource;
 #[ApiResource(
     normalizationContext: ['groups' => ['read:Action']],
     denormalizationContext: ['groups' => ['write:Action']]
-)]
+),
+ApiFilter(SearchFilter::class, properties: ['columnAction' => "exact"])]
 class Action
 {
     /**
